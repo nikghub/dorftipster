53bf47d9b0bd36dcf7418dc7254ada9d5be985ed
Niklas Löscher <niklas_loescher@hotmail.com>
2025-11-02 21:23:57 +0100

    Simplify TileSubsection

--- src/tile_subsection.py
+++ src/tile_subsection.py
@@ -12,32 +12,20 @@ class TileSubsection(IntEnum):
 
     @classmethod
     @lru_cache(maxsize=None)
-    def _all_values(cls):
-        return tuple(cls.__members__.values())
-
-    @classmethod
-    @lru_cache(maxsize=None)
-    def _side_values(cls):
-        return tuple(cls._all_values()[:-1])
-
-    @classmethod
-    @lru_cache(maxsize=None)
-    def _name_to_index(cls):
-        return {name: i for i, name in enumerate(cls.__members__)}
-
-    @classmethod
     def get_all_values(cls):
-        return cls._all_values()
+        return tuple(cls)
 
     @classmethod
+    @lru_cache(maxsize=None)
     def get_side_values(cls):
-        return cls._side_values()
+        return tuple(v for v in cls if v != cls.CENTER)
 
     @classmethod
+    @lru_cache(maxsize=None)
     def get_index(cls, subsection):
-        return cls._name_to_index()[subsection.name]
+        return subsection.value
 
     @classmethod
+    @lru_cache(maxsize=None)
     def at_index(cls, index):
-        side_vals = cls._side_values()
-        return side_vals[index % len(side_vals)]
+        return cls(index % len(cls.get_side_values()))
